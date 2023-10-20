06b0187bdbf86aa2e5f145f83dcac19de90c8095
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-10-20 07:15:28 +0600

    shorcut (&& and ||)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 "use strict";
 
 // short circuiting (&& and ||)
+console.log(undefined || 0 || "" || null || false || "hello");
+console.log(true && "nazmul");
+
+const num = 0;
+const guest1 = num ? num : 10;
+console.log(guest1);
