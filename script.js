b6396e1746cc43533620678f3027a576cd276f8d
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 15:14:11 +0600

    solve challenge one four problem

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // Coding Challenge #1
 
 /* 
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
@@ -71,3 +70,8 @@ const [gk, ...fieldPlayers] = players1;
 
 const allPlayers = [...players1, ...players2];
 // console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+const players1Final = ["Thiago", "Coutinho", "Perisic", ...players1];
+// console.log(players1Final);
