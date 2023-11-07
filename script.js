255b4593ebf0e6d4329f35c337377f40d8719f65
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 15:10:21 +0600

    solve challenge one two problem

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 // Coding Challenge #1
 
 /* 
-
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
@@ -63,4 +61,9 @@ const game = {
 // 1. Create one player array for each team (variables 'players1' and 'players2')
 
 const [players1, players2] = game.players;
-console.log(players1, players2);
+// console.log(players1, players2);
+
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
