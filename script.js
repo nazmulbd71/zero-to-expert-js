86f7626af96ca2fa3d8c090b1ce274e8bd18a532
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 15:19:18 +0600

    solve challenge one six problem

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 // Coding Challenge #1
 
 /* 
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
 TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
@@ -75,3 +73,16 @@ const allPlayers = [...players1, ...players2];
 
 const players1Final = ["Thiago", "Coutinho", "Perisic", ...players1];
 // console.log(players1Final);
+
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+const printGoals = function (...players) {
+  console.log(`total goal ${players.length}`);
+};
+
+printGoals(...game.scored);
