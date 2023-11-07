1ca47a6016527a797fb8d7ec3ca3e76ea0e1a494
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 17:07:45 +0600

    solve challenge four problem

--- script.js
+++ script.js
@@ -173,7 +173,57 @@ console.log(`An event happened, on average, every ${time / gameEvents.size}`);
 // [FIRST HALF] 17: ⚽️ GOAL
 
 for (const [time, result] of gameEvents.entries()) {
-  time <= 45
-    ? console.log(`[FIRST HALF] ${time}: ${result}`)
-    : console.log(`[SECOND HALF] ${time}: ${result}`);
+  // time <= 45
+  //   ? console.log(`[FIRST HALF] ${time}: ${result}`)
+  //   : console.log(`[SECOND HALF] ${time}: ${result}`);
 }
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const texts = document
+    .querySelector("textarea")
+    .value.toLowerCase()
+    .split("\n");
+  for (const [index, text] of texts.entries()) {
+    const [first, second] = text.split("_");
+    const output = `${first.trim()}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+
+    console.log(`${output.padEnd(20)}${"✅".repeat(index + 1)}`);
+  }
+});
