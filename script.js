3a02037a4f30eb9367880b529fde5035874f9f01
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 15:11:55 +0600

    solve challenge one three problem

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // Coding Challenge #1
 
 /* 
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
@@ -67,3 +66,8 @@ const [players1, players2] = game.players;
 
 const [gk, ...fieldPlayers] = players1;
 // console.log(gk, fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
