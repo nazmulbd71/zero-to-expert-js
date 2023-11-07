ecc3ee5efeb27e8c9a10e07d1dbee11151624a64
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 15:07:26 +0600

    solve first problem

--- script.js
+++ script.js
@@ -1,108 +1,17 @@
 "use strict";
 
-// short circuiting (&& and ||)
-// console.log(undefined || 0 || "" || null || false || "hello");
-
-const num = 0;
-const guest1 = num ? num : 10;
-
-// shortcut (but only execute default when first one is false so 0 is false value)
-const guest2 = num || 10;
-// console.log(guest2);
-
-// shortcut && (only execute default when left side true)
-// console.log(true && "nazmul");
-
-//end short circuiting (&& and ||)
-
-// The Nullish Coalescing operator (??)
-// Note: if null and undefined then it execute default but not (0, '');
-const guest3 = num ?? 20;
-// console.log(guest3);
-
-//End The Nullish Coalescing operator (??)
-
-// Logical assignment operator
-const res1 = {
-  name: "nazmul",
-  age: 0,
-};
-
-const res2 = {
-  name: "jannatul",
-  age: 28,
-  husbandName: "nazmul",
-};
-
-// res1.age = res1.age || 31;
-// res1.age ||= 31;
-
-res1.age ??= 31;
-// console.log(res1.age);
-
-res2.husbandName &&= "Yes iam";
-// console.log(res2.husbandName);
-
-//End Logical assignment operator
-
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
-
-// Looping Objects: Object Keys,Values, Entries
-
-const { openingHours } = restaurant;
-
-const propertyName = Object.keys(openingHours);
-// console.log(propertyName);
-
-const eachValue = Object.values(openingHours);
-// console.log(eachValue);
-
-const propertyWithValues = Object.entries(openingHours);
-// console.log(propertyWithValues);
-for (const [property, { open, close }] of propertyWithValues) {
-  //   console.log(property, open, close);
-}
-
-// Looping Objects: Object Keys,Values, Entries
+// Coding Challenge #1
 
 /* 
-Let's continue with our football betting app!
 
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-
-3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
-
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
 GOOD LUCK 😀
 */
@@ -148,20 +57,10 @@ const game = {
   },
 };
 
-// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-for (let i = 0; i < game.scored.length; i++) {
-  //   console.log(`Goal ${i + 1}: ${game.scored[i]}`);
-}
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-for (const [i, plyer] of game.scored.entries()) {
-  //   console.log(`Goal ${i + 1}: ${plyer}`);
-}
+// 1. Create one player array for each team (variables 'players1' and 'players2')
 
-// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-
-const odds = Object.values(game.odds);
-// console.log(odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-// console.log(average);
+const [players1, players2] = game.players;
+console.log(players1, players2);
